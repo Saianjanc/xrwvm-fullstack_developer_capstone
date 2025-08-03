9a00c74fa0a4a6626040a74028aeafe15cd34c90
Saianjanc <saianjan1234@gmail.com>
2025-08-03 08:32:37 -0400

    deployed the app using docker

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,13 +28,15 @@ SECRET_KEY = (
 DEBUG = True
 
 ALLOWED_HOSTS = [
-        'localhost',
-        'https://saianjan1234-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'localhost',
+    'saianjan1234-8000.theiadockernext-1-labs-prod-'
+    'theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-        'http://localhost:8000',
-        'https://saianjan1234-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'http://localhost:8000',
+    'https://saianjan1234-8000.theiadockernext-1-labs-prod-'
+    'theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 REST_FRAMEWORK = {
