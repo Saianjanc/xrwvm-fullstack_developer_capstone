c60af2be7b2201aac8cf3cf38f31b5715e81997f
Saianjanc <saianjan1234@gmail.com>
2025-08-03 07:36:42 -0400

    pylint changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,13 +26,13 @@ urlpatterns = [
         name='dealer_details'
     ),
     path(
-    'reviews/dealer/<int:dealer_id>',
-    views.get_dealer_reviews,
-    name='dealer_reviews'
+        'reviews/dealer/<int:dealer_id>',
+        views.get_dealer_reviews,
+        name='dealer_reviews'
     ),
     path('add_review', views.add_review, name='add_review'),
     path('get_cars', views.get_cars, name='getcars'),
 ] + static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
-    )
+)
